00c531cf1454e60513c508267e7bec700ecee6b4
Will Duquette <will@wjduquette.com>
2014-10-27 18:47:43 -0700

    Cleaned up markupparser(n).

--- lib/markupparser/lshift.tcl
+++
@@ -1,1 +0,0 @@
-  proc lshift {listvar} {      upvar $listvar args            if {[llength $args] == 0} {          set arg ""      } elseif {[llength $args] == 1} {          set arg [lindex $args 0]          set args ""      } else {          set arg [lindex $args 0]          set args [lrange $args 1 end]      }            return $arg  }
